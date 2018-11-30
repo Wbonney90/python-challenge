13dbfd7a3e0a2b80d457118bc9607c7c3432e2ec
Bill Bonney <wbonney90@gmail.com>
2018-11-30 13:51:40 -0500

    PUSH

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,61 +1,129 @@
-#+![Revenue](Images/revenue-per-lead.jpg)
+#import os
+#import csv
 
-#* In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company.
-# You will give a set of financial data called [budget_data.csv](PyBank/Resources/budget_data.csv).
-# The dataset is composed of two columns: `Date` and `Profit/Losses`.
-# (Thankfully, your company has rather lax standards for accounting so the records are simple.)
+#total = 0
 
-import os
-import csv
-
-total = 0
-
-csvpath = os.path.join("..","PyBank","budget_data.csv")
-with open(csvpath, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+#svpath = os.path.join("..","PyBank","budget_data.csv")
+#with open(csvpath, newline="") as csvfile:
+    #csvreader = csv.reader(csvfile, delimiter=",")
     #print(csvreader)
 
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
 
-    # * The total number of months included in the dataset
-    numline=len(csvfile.readlines())
-    print("Total Month: ", numline)
+    #print("Financial Anaylsis")
+    #print("----------------------------")
 
-    # * The total net amount of "Profit/Losses" over the entire period
-with open(csvpath, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    net_profit=[]
-    for rows in csvreader:
-        net_profit.append(rows[1])
-    net_profit.remove("Profit/Losses")
-    net_profit = [int(rows) for rows in net_profit]
-    total_net = ("Total: $" + str(sum(net_profit)))
-    print(total_net)
+    #The total number of months included in the dataset
+    #numline=len(csvfile.readlines())
+    #tot_month = ("Total Month: " + str(numline))
+    #print(tot_month)
+
+    #The total net amount of "Profit/Losses" over the entire period
+#with open(csvpath, newline="") as csvfile:
+    #csvreader = csv.reader(csvfile, delimiter=",")
+    #net_profit=[]
+    #list_of_av_change = []
+    #reat_inc_value = 0
+    #great_dec_value = 99999999
+    #for rows in csvreader:
+        #net_profit.append(rows[1])
+    #net_profit.remove("Profit/Losses")
+    #net_profit = [int(rows) for rows in net_profit]
+    #total_net = ("Total: $" + str(sum(net_profit)))
+    #print(total_net)
 
     #Failed attempt to total column 2
     #for row in csv.reader(csvfile):
         #total += float(row[column2])
     #print(total)
 
-  #* The average change in "Profit/Losses" between months over the entire period
+    #The average change in "Profit/Losses" between months over the entire period
+
+    #copy of Charlotte's code to analyze
+    #av_change = [y - x for x, y in zip(net_profit, net_profit[1:])]
+    #import statistics
+    #x = statistics.mean(av_change)
+    # round(x,2) returns a rounded number to the second decimal point
+    #Average_Change = (f"Average Change: ${round(x, 2)}")
+    #list_of_av_change.append(x)
+    #print(Average_Change)
+    #print(list_of_av_change)
+    #print(x)
+
+# * The greatest increase in profits (date and amount) over the entire period
+# * The greatest decrease in losses (date and amount) over the entire period
+
+#f = open(csvpath, 'r')
+#reader = csv.reader(f)
+#months_profit = {}
+#for row in reader:
+    #months_profit[row[0]] = {'month':row[0],'profit':row[1]}
+
+    #if list_of_av_change > great_inc_value:
+        #high = max(av_change)
+        #date_of_greatest_inc = rows[0]
+    #elif list_of_av_change < great_dec_value:
+        #low = min(av_change)
+        #date_of_greatest_dec = rows[0]
+    #great_dec = ("Greatest Decrease in Profits: $" + str(low))
+    #great_inc = ("Greatest Increase in Profits: $" + str(high))
+    #print(date_of_greatest_inc)
+    #print(date_of_greatest_dec)
+    #print(great_dec)
+
+#def writetofile():
+    #output = "Financial_Analysis.txt"
+    #Fin = "Financial Analysis"
+    #Break = "----------------------------"
+    #file = open(output,"w")
+    #print(great_inc)
+    #file.write(Fin + '\n')
+    #file.write(Break + '\n')
+    #file.write(tot_month + '\n')
+    #file.write(total_net + '\n')
+    #file.write(Average_Change + '\n')
+    #file.write(great_inc + '\n')
+    #file.write(great_dec + '\n')
+    #file.close()
+#writetofile()
 
 
+import os
+import csv
 
-  #* The greatest increase in profits (date and amount) over the entire period
+budget_data_sheet = "budget_data.csv"
+budget_result_sheet = "budget_analysis.txt"
 
-  #* The greatest decrease in losses (date and amount) over the entire period
 
-#* As an example, your analysis should look similar to the one below:
-
-  #```text
-  #Financial Analysis
-  #----------------------------
-  #Total Months: 86
-  #Total: $38382578
-  #Average  Change: $-2315.12
- # Greatest Increase in Profits: Feb-2012 ($1926159)
-  #Greatest Decrease in Profits: Sep-2013 ($-2196167)
-  #```
-
-#* In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+with open(budget_data_sheet, newline="") as budget_data_fh:
+    fh_reader = csv.reader(budget_data_fh, delimiter=',')
+    header = next(fh_reader)
+    bank_data_list = []
+    prev_month = 0
+    months = 0
+    total = 0
+    for row in fh_reader:
+        bank_data_list.append(row)
+        if months == 0:
+            bank_data_list[months].append(0)
+        elif months > 0:
+            bank_data_list[months].append(int(bank_data_list[months][1]) - prev_month)
+            prev_month = int(bank_data_list[months][1])
+        total += int(row[1])
+        months += 1
+    list_profit_loss = []
+    total_change = 0.0
+    for counter in range(0,(months - 1)):
+        list_profit_loss.append(int(bank_data_list[counter][2]))
+        total_change += list_profit_loss[counter]
+print(bank_data_list)
+max_profit = max(list_profit_loss)
+max_loss = min(list_profit_loss)
+avg_change = total_change/months
+print(f"Net amount of Profit/Loss: {total}\n")
+print(f"Total months in balance sheet: {months}\n")
+print(f"Max Profit: {max_profit}\n")
+print(f"Max Loss: {max_loss}\n")
+print(f"Average Change: {avg_change}\n")
+budget_data_fh.close()
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,51 +1,85 @@
-#+## PyPoll
+#import os
+#import csv
 
-#![Vote-Counting](Images/Vote_counting.jpg)
+#csvpath = os.path.join("..","PyPoll","election_data.csv")
+#with open(csvpath, newline="") as csvfile:
+    #csvreader = csv.reader(csvfile, delimiter=",")
+    #next(csvreader)
 
-#* In this challenge, you are tasked with helping a small, rural town modernize its vote-counting process. (Up until now, Uncle Cleetus had been trustfully tallying them one-by-one, but unfortunately, his concentration isn't what it used to be.)
+#candidates =["",0]
+    #for rows in csvreader:
+       # print(rows)
+        #numline = len(csvfile.readlines())
+        #tot_votes = ("Total Votes: " + str(numline))
+    #print(tot_votes)
+# import libraries
 
-#* You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+import csv
+import os
 
-  #* The total number of votes cast
+# Files to load and output (Remember to change these)
+file_to_load = os.path.join("election_data.csv")
+file_to_output = os.path.join("election_analysis.txt")
 
-  #* A complete list of candidates who received votes
+total_votes = 0
 
-  #* The percentage of votes each candidate won
+candidate_options = []
+candidate_votes = {}
 
-  #* The total number of votes each candidate won
+winning_candidate = ""
+winning_count = 0
 
-  #* The winner of the election based on popular vote.
+with open(file_to_load) as election_data:
+    reader = csv.reader(election_data)
 
-#* As an example, your analysis should look similar to the one below:
+    header = next(reader)
 
-  #```text
-  #Election Results
-  #-------------------------
-  #Total Votes: 3521001
-  #-------------------------
-  #Khan: 63.000% (2218231)
-  #Correy: 20.000% (704200)
-  #Li: 14.000% (492940)
-  #O'Tooley: 3.000% (105630)
-  #-------------------------
-  #Winner: Khan
-  #-------------------------
-  #```
+    for row in reader:
 
-#* In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+        print(". ", end=""),
 
-## Hints and Considerations
+        total_votes = total_votes + 1
 
-#* Consider what we've learned so far. To date, we've learned how to import modules like `csv`; to read and write files in various formats; to store contents in variables, lists, and dictionaries; to iterate through basic data structures; and to debug along the way. Using what we've learned, try to break down you tasks into discrete mini-objectives. This will be a _much_ better course of action than attempting to Google Search for a miracle.
+        candidate_name = row[2]
 
-#* As you will discover, for some of these challenges, the datasets are quite large. This was done purposefully, as it showcases one of the limits of Excel-based analysis. While our first instinct, as data analysts, is often to head straight into Excel, creating scripts in Python can provide us with more robust options for handling "big data".
+        if candidate_name not in candidate_options:
 
-#* Your scripts should work for each dataset provided. Run your script for each dataset separately to make sure that the code works for different data.
+            candidate_options.append(candidate_name)
 
-#* Feel encouraged to work in groups, but don't shortchange yourself by copying someone else's work. You get what you put in, and the art of programming is extremely unforgiving to moochers. Dig your heels in, burn the night oil, and learn this while you can! These are skills that will pay dividends in your future career.
+            candidate_votes[candidate_name] = 0
 
-#* Start early, and reach out for help often! Challenge yourself to identify _specific_ questions for your instructors and TAs. Don't resign yourself to simply saying, "I'm totally lost." Come prepared to show your effort and thought patterns, we'll be happy to help along the way.
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
 
-#* Always commit your work and back it up with GitHub pushes. You don't want to lose hours of your work because you didn't push it to GitHub every half hour or so.
 
-  #* **Commit often**.
+with open(file_to_output, "w") as txt_file:
+
+    election_results = (
+        f"\n\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+
+    txt_file.write(election_results)
+
+    for candidate in candidate_votes:
+
+        votes = candidate_votes.get(candidate)
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        print(voter_output, end="")
+
+        txt_file.write(voter_output)
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+
+    txt_file.write(winning_candidate_summary)
